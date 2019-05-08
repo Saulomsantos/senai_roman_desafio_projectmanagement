e18931df86a927bb81576ee03cc102f25b693d1d
Gabriel Cardoso Martins <45346351841@info.local>
2019-05-08 11:20:54 -0300

    Endpoints feitos

--- backEnd/RomanApi/RomanApi/Repositorys/ProjetoRepository.cs
+++ backEnd/RomanApi/RomanApi/Repositorys/ProjetoRepository.cs
@@ -1,7 +1,10 @@
-﻿using RomanApi.Domains;
+﻿using Microsoft.EntityFrameworkCore;
+using RomanApi.Domains;
 using RomanApi.Interfaces;
+using RomanApi.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,12 +42,46 @@ namespace RomanApi.Repositorys
             }
         }
 
-        public List<Projetos> ListarProjetos()
+        public List<ProjetoViewModel> ListarProjetos()
         {
-            using (RomanContext ctx = new RomanContext())
+            string stringConexao = "Data Source=romandb.database.windows.net; Initial Catalog=romanbd; user id=roman-admin; pwd=Batata123;";
+
+            string query = "SELECT P.ID, NOME, DESCRICAO, TEMA FROM PROJETOS P LEFT JOIN TEMAS T ON P.TEMA_ID = T.ID";
+
+            List<ProjetoViewModel> projetos = new List<ProjetoViewModel>();
+
+            using(SqlConnection con = new SqlConnection(stringConexao))
             {
-                return ctx.Projetos.ToList();
+                con.Open();
+                SqlDataReader sdr;
+
+                using(SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    sdr = cmd.ExecuteReader();
+
+                    if(sdr.HasRows)
+                    {
+                        while(sdr.Read())
+                        {
+                            ProjetoViewModel projeto = new ProjetoViewModel()
+                            {
+                                Id = Convert.ToInt32(sdr["ID"]),
+                                Nome = sdr["NOME"].ToString(),
+                                Descricao = sdr["DESCRICAO"].ToString(),
+                                Tema = sdr["TEMA"].ToString()
+                            };
+
+                            projetos.Add(projeto);
+                        }
+
+                        return projetos;
+                    }
+                }
             }
+
+            return null;
+
+
         }
     }
 }
