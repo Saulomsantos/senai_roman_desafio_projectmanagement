42cea68fce7c1a22c633f0f51c909c3f287e9cf9
Saulo Santos <saulo.macedoprojetos@gmail.com>
2019-05-09 11:19:13 -0300

    Adicionando estrutura gerenciar tipos usuarios

--- frontEnd/RNRoman/src/pages/gerenciartipousuario.js
+++ frontEnd/RNRoman/src/pages/gerenciartipousuario.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { FlatList, Text, View, StyleSheet } from 'react-native';
+
+import api from '../services/api';
 
 class GerenciarTipoUsuario extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            listaTiposUsuarios: []
+        }
+    }
+
+    componentDidMount() {
+        this._carregaTiposUsuarios();
+    }
+
+    _carregaTiposUsuarios = async () => {
+        const resposta = await api.get('/Usuarios/tipo');
+        const dadosDaApi = resposta.data;
+        this.setState({ listaTiposUsuarios : dadosDaApi })
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Gerenciar Tipo Usuario</Text>
+                <FlatList
+                    contentContainerStyle={styles.ConteudoLista}
+                    data={this.state.listaTiposUsuarios}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderizaItem}
+                />
             </View>
         )
     }
+
+    renderizaItem = ({ item }) => (
+        <View style={styles.flatItemSeparador}>
+            <View style={styles.flatItemContainer}>
+                <Text style={styles.flatItemLinha}>{item.Id} - {item.TipoUsuario}</Text>
+            </View>
+        </View>
+      );
 }
 
 export default GerenciarTipoUsuario;
@@ -20,4 +53,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    // conteúdo da lista
+    ConteudoLista: {
+    paddingTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50
+  },
+    // dados do evento de cada item da linha
+    flatItemSeparador: {
+    flexDirection: "row",
+    borderBottomWidth: 0.9,
+    borderBottomColor: "gray"
+  },
 });
