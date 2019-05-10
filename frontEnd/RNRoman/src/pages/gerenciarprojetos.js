26acb29e2547f278e0f40a817e315950f3b87342
Gabriel Cardoso <gabriel.cmartins11@gmail.com>
2019-05-10 01:48:24 -0300

    Adicionando a listagem de projetos

--- frontEnd/RNRoman/src/pages/gerenciarprojetos.js
+++ frontEnd/RNRoman/src/pages/gerenciarprojetos.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { AsyncStorage, Text, View, StyleSheet, FlatList } from 'react-native';
+import api from '../services/api';
 
 class GerenciarProjetos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listaProjetos: [],
+            token: ""
+        }
+    }
+
+    _BuscarDados = async () => {
+        try {
+            console.warn("Buscar Dados comecou")
+          const value = await AsyncStorage.getItem('userToken');
+          if (value !== null) {
+            this.setState({ token: value });
+          }
+        } catch (error) {}
+
+        this._carregaProjetos();
+        console.warn("Buscar Dados foi")
+        console.warn(this.state.token)
+      };
+    
+    _carregaProjetos = async () => {
+        console.warn("Carregar Projetos Comecou")
+        const resposta = await api.get("/Projetos", {
+            headers: {
+                'Authorization' : 'Bearer ' + (this.state.token)
+            }
+        }
+        );
+        const dadosApi = resposta.data
+        this.setState({listaProjetos: dadosApi})
+        console.warn("Carregar Projetos foi")
+    }
+
+    componentDidMount() {
+        this._BuscarDados();
+        console.warn("Opa")
+    }
+    
     render() {
         return (
             <View style={styles.container}>
-                <Text>Gerenciar Projetos</Text>
+                <Text style={paddingTop= 15}>{"Projetos".toUpperCase()}</Text>
+                <FlatList
+                    contentContainerStyle={styles.ConteudoLista}
+                    data={this.state.listaProjetos}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderizaItem}
+                />
             </View>
         )
     }
+
+    renderizaItem = ({ item }) => (
+        <View style={styles.flatItemSeparador}>
+            <View style={styles.flatItemContainer}>
+                <Text style={styles.flatItemLinha}>{item.id} - {item.nome}</Text>
+            </View>
+        </View>
+      );
 }
 
 export default GerenciarProjetos;
@@ -20,4 +75,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    // linha de separacao do cabecalho
+    mainHeaderLine: {
+        width: 170,
+        paddingTop: 10,
+        borderBottomColor: "#999999",
+        borderBottomWidth: 0.9
+    },
+    // conteúdo da lista
+    ConteudoLista: {
+    paddingTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50
+  },
+    // dados do evento de cada item da linha
+    flatItemSeparador: {
+    flexDirection: "row",
+    borderBottomWidth: 0.9,
+    borderBottomColor: "gray",
+    marginBottom: 10
+  },
 });
