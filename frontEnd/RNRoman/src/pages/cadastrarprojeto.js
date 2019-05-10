4213dd148ee0df1faeb1af6d8d92acd0b5b10062
Gabriel Cardoso Martins <45346351841@info.local>
2019-05-10 11:24:54 -0300

    Cadastrar projeto feito

--- frontEnd/RNRoman/src/pages/cadastrarprojeto.js
+++ frontEnd/RNRoman/src/pages/cadastrarprojeto.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { AsyncStorage, Text, View, StyleSheet, Button } from 'react-native';
+import api from '../services/api';
+import { Input } from 'native-base';
+import { TextInput } from 'react-native-gesture-handler';
 
 class CadastrarProjeto extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: "",
+            nome: "",
+            descricao: "",
+            temaid: "1"
+        }
+    }
+
+    _BuscarTokem = async() => {
+        const value = await AsyncStorage.getItem('userToken')
+        if (value !== null) {
+            this.setState({token: value})
+        }
+        this._CadastrarProjeto()
+    }
+    
+    _CadastrarProjeto = async () =>{
+        const resposta = await api.post
+        (   
+            'projetos',
+            {
+                nome: this.state.nome,
+                descricao: this.state.descricao,
+                temaid: this.state.temaid
+            },
+            {
+                headers: {
+                    'Authorization' : 'Bearer ' + (this.state.token)
+                }
+            }
+        )
+    }
+    
     render() {
         return (
             <View style={styles.container}>
-                <Text>Cadastrar Projeto</Text>
+                <Text>Coé</Text>
+                <TextInput
+                    placeholder="Nome do projeto"
+                    onChangeText={nome => this.setState({nome})}
+                />
+                <TextInput
+                    placeholder="Descrição"
+                    onChangeText={descricao => this.setState({descricao})}
+                />
+                <Button title="Cadastrar" onPress={this._BuscarTokem}/>
             </View>
         )
     }
@@ -21,74 +68,3 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import { StyleSheet, Text, View, Button } from 'react-native';
-// import { TextInput } from 'react-native-gesture-handler';
-// // import { Button } from 'native-base';
-
-// export default class CadastrarProjeto extends Component {
-//     // constructor() {
-//     //     super();
-//     //     this.state = {
-//     //         nome: "",
-//     //         descricao: ""
-//     //     }
-//     // }
-
-//     // componentDidUpdate() {
-//         // console.warn(this.state.nome)
-//         // console.warn(this.state.email) }
-
-
-//     // teste(e) {
-//     //     //  this.setState({nome: e.target.value})
-//     //     //  this.setState({nome: e})
-//     //     console.warn(this.state.nome)
-//     //     console.warn(this.state.email)
-//     // }
-
-//     render() {
-//         return (
-//             <View style={styles.container}>
-//                 <Text>Cadastrar tema</Text>
-//                 {/* <TextInput
-//                     style={styles.input}
-//                     onChangeText={(text) => this.setState({ nome: text })}
-//                     // onChangeText={this.teste.bind(this)}
-//                     placeholder="Nome do projeto"
-//                 />
-//                 {/* <TextInput
-//                     style={styles.input}
-//                     onChangeText={(text) => this.setState({ email: text })}
-//                     placeholder="Descrição do projeto"
-//                 />
-//                 <Button title="Opa"/> */}
-//             </View>
-//         )
-//     }
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-
-//     input: {
-//         height: 64,
-//         width: 256,
-//         borderColor: 'gray',
-//         borderWidth: 2,
-//         marginBottom: 25,
-//         borderRadius: 32,
-//         paddingLeft: 16
-//     }
-// });
